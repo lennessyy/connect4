a17eff36eb3008200371e33fca1396965c3dddb7
lennessyy <jiaxichen20@gmail.com>
2020-05-21 09:50:57 -0700

    added tests for diagnal win

--- connect4.test.js
+++ connect4.test.js
@@ -1,4 +1,10 @@
 describe('checks for end game', function (){
+	beforeEach(function (){
+		for (let row of board) {
+			row.fill(null);
+		}
+	});
+
 	it('should be able to check horizontal', function (){
 		for (let i = 0; i < 4; i++) {
 			board[5][i] = 1;
@@ -10,6 +16,14 @@ describe('checks for end game', function (){
 		for (let i = 1; i < 5; i++) {
 			board[i][3] = 1;
 		}
+		expect(checkForWin()).toEqual(true);
+	});
+
+	it('should be able to check diagnal', function (){
+		board[0][0] = 1;
+		board[1][1] = 1;
+		board[2][2] = 1;
+		board[3][3] = 1;
 		expect(checkForWin()).toEqual(true);
 	});
 
